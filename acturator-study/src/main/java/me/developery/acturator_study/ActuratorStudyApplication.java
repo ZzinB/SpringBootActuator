be9a6005874dada452e3faba290414db224cd9b5
ZzinB <mysteleee7@gmail.com>
2024-10-03 15:12:28 +0900

    Settings: 프로젝트 생성

---
+++ acturator-study/src/main/java/me/developery/acturator_study/ActuratorStudyApplication.java
@@ -0,0 +1,13 @@
+package me.developery.acturator_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuratorStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuratorStudyApplication.class, args);
+	}
+
+}
---
+++ acturator-study/src/test/java/me/developery/acturator_study/ActuratorStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.developery.acturator_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuratorStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
